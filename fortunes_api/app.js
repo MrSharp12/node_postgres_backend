65d20d8f1771f44d540625e697b124eabc5bcd84
The Sharps <Sharp@Thes-Air.attlocal.net>
2018-02-10 21:59:20 -0500

    Fortunes POST method

--- fortunes_api/app.js
+++ fortunes_api/app.js
@@ -1,21 +1,67 @@
-
-const express = require('express');//this allows us to make the express application
-const fortunes = require('./data/fortunes')
+const fs = require('fs');
+const express = require('express');//allow us to make the express application
+const bodyParser = require('body-parser');
+const fortunes = require('./data/fortunes');
 
 const app = express();//calls the express function
 
+app.use(bodyParser.json());
+
 app.get('/fortunes', (req, res) => {
-    res.json(fortunes);//.json is an express method, for responding and rendering with JSON formated data
+  res.json(fortunes);//.json is an express method, for responding and rendering with JSON formated data
 });
 
 app.get('/fortunes/random', (req, res) => {
-    res.json(fortunes[Math.floor(Math.random() * fortunes.length)]);//app responds as json for the request
+  res.json(fortunes[Math.floor(Math.random() * fortunes.length)]);//app responds as json for the request
 });
-//find is an array function
+
 app.get('/fortunes/:id', (req, res) => {
-    res.json(fortunes.find(f => f.id == req.params.id));//don't use strict because of JSON object values are strings
+  res.json(fortunes.find(f => f.id == req.params.id));//don't use strict because of JSON object values are strings
+});
+
+const writeFortunes = json => {
+  fs.writeFile('./data/fortunes.json', JSON.stringify(json), err => console.log(err));
+};
+
+app.post('/fortunes', (req, res) => {
+  const { message, lucky_number, spirit_animal } = req.body;
+
+  const fortune_ids = fortunes.map(f => f.id);
+
+  const new_fortunes = fortunes.concat({
+    id: (fortune_ids.length > 0 ? Math.max(...fortune_ids) : 0) + 1,
+    message,
+    lucky_number,
+    spirit_animal
+  });
+
+  writeFortunes(new_fortunes);
+
+  res.json(new_fortunes);
+});
+
+app.put('/fortunes/:id', (req, res) => {
+  const { id } = req.params;
+
+  const old_fortune = fortunes.find(f => f.id == id);
+
+  ['message', 'lucky_number', 'spirit_animal'].forEach(key => {
+    if (req.body[key]) old_fortune[key] = req.body[key];
+  });
+
+  writeFortunes(fortunes);
+
+  res.json(fortunes);
+});
+
+app.delete('/fortunes/:id', (req, res) => {
+  const { id } = req.params;
+
+  const new_fortunes = fortunes.filter(f => f.id != id);
+
+  writeFortunes(new_fortunes);
+
+  res.json(new_fortunes);
 });
 
 module.exports = app;
-
-
